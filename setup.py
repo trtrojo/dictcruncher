ec1450d85b333b750d0ec4bdb5a4fa6c6c71b88e
trojo <trojo@bakery>
2023-12-13 02:01:07 -0500

    add description for pypi, added build directories for .gitignore

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='dictcruncher',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/trtrojo/dictcruncher',
     license='LICENSE',
     author='Tommy Rojo',
     author_email='tr.trojo@gmail.com',
     description='A library for flattening json objects',
+    long_description = open("README.md").read(),
+    long_description_content_type='text/markdown',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
